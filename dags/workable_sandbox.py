5e93e38cd4912a527b91a8f25f0eea56eebec2cb
Willi <willi.mueller@posteo.de>
2024-03-25 18:30:26 +0530

    renames greenhouse, adds workable dag, adds greenhouse with XCOM to get connection secrets

--- dags/workable_sandbox.py
+++ dags/workable_sandbox.py
@@ -1,5 +1,5 @@
 '''
-## Example Pod DAG
+## Workable Sandbox DAG
 
 Sample dag to import into K8S
 '''
@@ -8,7 +8,6 @@ import json
 from airflow import DAG
 from airflow.configuration import conf
 from airflow.hooks.base import BaseHook
-from airflow.models import Connection
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
     KubernetesPodOperator,
 )
@@ -29,13 +28,11 @@ else:
 
 
 workable_connection = BaseHook.get_connection("workable_eqtble_sandbox")
-greenhouse_connection = BaseHook.get_connection("greenhouse_eqtble_sandbox")
 
 snowflake_connection = SnowflakeHook.get_connection("snowflake_sandbox")
 snowflake_extra = json.loads(snowflake_connection.get_extra())
 
 env_vars = {
-    "SOURCES__GREENHOUSE__ACCESS_TOKEN": greenhouse_connection.password,
     "SOURCES__WORKABLE__ACCESS_TOKEN": workable_connection.password,
     "SOURCES__WORKABLE__SUBDOMAIN": workable_connection.host,
     "DESTINATION__SNOWFLAKE__CREDENTIALS__DATABASE": snowflake_extra.get("database"),
@@ -47,7 +44,7 @@ env_vars = {
 }
 
 with DAG(
-    dag_id="greenhouse_eqtble_sandbox",
+    dag_id="workable_sandbox",
     start_date=datetime(2024, 1, 1),
     schedule="@daily",
     catchup=False,
@@ -61,7 +58,7 @@ with DAG(
         image="ghcr.io/untitled-data-company/eqtable-dlt:main",
         image_pull_secrets=[k8s.V1LocalObjectReference("ghcr-login-secret")],
         # labels={"<pod-label>": "<label-name>"},
-        name="airflow-test-pod",
+        name="airflow-workable",
         task_id="task-one",
         in_cluster=in_cluster,  # if set to true, will look in the cluster, if false, looks for file
         cluster_context="docker-desktop",  # is ignored when in_cluster is set to True
@@ -69,6 +66,6 @@ with DAG(
         is_delete_operator_pod=False,
         get_logs=True,
         image_pull_policy="IfNotPresent",  # crucial to avoid pulling image from the non-existing local registry
-        env_vars=init(),
-        arguments=["greenhouse_pipeline.py"],
+        env_vars=env_vars,
+        arguments=["workable_pipeline.py"],
     )
